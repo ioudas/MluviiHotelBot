a41ebd6dd21d9b63550609d3288041f64f2b43fe
Petr Dobroucky <petr.dobroucky@mluvii.com>
2019-02-08 15:55:47 +0100

    added mluvii channel

---
+++ MluviiChannel/MluviiChannel.cs
@@ -0,0 +1,23 @@
+﻿using AdaptiveCards;
+using Microsoft.Bot.Builder;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BasicBot.MluviiChannel
+{
+    public static class MluviiChannel
+    {
+        public static async Task<Dictionary<string, string>> GetCallParams(ITurnContext context)
+        {
+            var data = JObject.Parse(@"{ ""Activity"": ""GetCallParams"" }");
+            var act = context.Activity.CreateReply();
+            act.ChannelData = data;
+            var callparams = await context.SendActivityAsync(act);
+
+            throw new NotImplementedException();
+        }
+    }
+}
