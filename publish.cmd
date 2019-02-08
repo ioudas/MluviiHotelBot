72a8ecc00949a5e9f89d71f26c96bec31cd3a51c
Petr Dobroucky <petr.dobroucky@mluvii.com>
2019-02-08 13:41:54 +0100

    sample bot

---
+++ publish.cmd
@@ -0,0 +1,3 @@
+nuget restore
+msbuild BasicBot.sln -p:DeployOnBuild=true -p:PublishProfile=mluviihotelbot-Web-Deploy.pubxml -p:Password=rdqDrmqqTje56BZAa1PGjqSyfCSjNl3m7DswANcMCZHCQ9Bb73TEtn5ZqXdv
+
